8c46aed7b0206920d596c5d38e1ef9b8bbe33b62
lesly <notter.lesly@gmail.com>
2022-12-03 19:03:41 +0100

    first commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,32 @@
+<template id="TheFooter">
+    <nav class="contacts">
+        <ul v-for="contact in contacts" :key="contact" >
+            <li><a :href="contact.link">{{contact.site}}</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+
+const contacts = [
+{'site':'GitHub', 'link': 'https://github.com/notter-lesly'},
+{'site':'Linkedin', 'link':'https://www.linkedin.com/in/leslynotter/'},
+{'site':'Whatsapp', 'link':'https://api.whatsapp.com/send?phone=310627586099'}
+]
+
+
+</script>
+
+<style>
+.contacts{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.contacts a{
+cursor:pointer,
+
+}
+
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="header">
+        <h1 class="title"> Lesly Notter</h1>
+        <h3 class="subtitle"> Front End Developer</h3>
+    </div>
+    <nav class="menu">
+    <nuxtLink to="/About"> About </nuxtLink>
+    <nuxtLink to="/skills"> Skills </nuxtLink>
+    <nuxtLink to="/projects"> Projects </nuxtLink>
+    <a @click="scrollFooter"> Contact  </a>
+    </nav>
+</template>
+
+<script setup>
+
+const emit = defineEmits(['event'])
+
+function scrollFooter(){
+   emit('event')
+}
+</script>
+<style scoped>
+
+.header{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    }
+
+.title{
+    font-size:20px;
+    margin: 1rem 0 1rem 0;
+    font-family: 'Tangerine';
+}
+
+.subtitle{
+    font-size:16px;
+    margin: 0.5rem 0 1rem 0;
+}
+
+a.nuxt-link{
+    color:black;
+}
+
+a.nuxt-link-active{
+    color:blueviolet;
+}
+
+.menu{
+    display: flex;
+    justify-content: space-around;
+} 
+
+.menu a{
+    cursor:pointer;
+    text-decoration: underline;
+}
+
+
+</style>
